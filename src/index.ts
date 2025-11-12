cec4b9bab3cea258b5589819d66724764cd463f3
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-12 21:06:10 +0600

    Issues with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  //   const result = await prisma.post.create({
-  //     data: {
-  //       title: "My first post",
-  //       content: "Hello world 2",
-  //       authorName: "Rakib Islam Rifat",
-  //     },
-  //   });
-  const getAllFromDB = await prisma.post.findMany();
-  console.log(getAllFromDB);
+  const result = await prisma.post.create({
+    data: {
+      title: "My first post",
+      content: "Hello world 3",
+      authorName: "Rakib Islam Rifat",
+    },
+  });
+  //   const getAllFromDB = await prisma.post.findMany();
+  //   console.log(getAllFromDB);
 };
 
 main();
