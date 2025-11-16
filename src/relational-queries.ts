82056f2296be11194a32329ca3f5d67cfc5b14f7
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-16 12:20:29 +0600

    Implementing Relation Filters

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -6,14 +6,25 @@ const prisma = new PrismaClient();
 
 // fluent way to query relational data
 const relationalQueries = async () => {
-  const result = await prisma.user
-    .findUnique({
-      where: {
-        id: 2,
+  //   const result = await prisma.user
+  //     .findUnique({
+  //       where: {
+  //         id: 2,
+  //       },
+  //     })
+  //         .post();
+
+  // relational filtering
+  const publishedPostUsers = await prisma.user.findMany({
+    include: {
+      post: {
+        where: {
+          published: true,
+        },
       },
-    })
-    .post();
-  console.log(result);
+    },
+  });
+  console.dir(publishedPostUsers, { depth: Infinity });
 };
 
 relationalQueries();
