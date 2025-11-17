90b571ec0eb5e8bb8894ca81d84071df096d1f21
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-17 11:49:02 +0600

    Group By and Filtering with Having Clause

---
+++ src/groupby.ts
@@ -0,0 +1,20 @@
+import { PrismaClient, UserRole } from "./generated/prisma/client.js";
+import "dotenv/config";
+const prisma = new PrismaClient();
+
+const groupBy = async () => {
+  const groupByPost = await prisma.post.groupBy({
+    by: ["authorId", "published"],
+    _count: {
+      title: true,
+    },
+    having: {
+      authorId: {
+        gt: 1,
+      },
+    },
+  });
+  console.log(groupByPost);
+};
+
+groupBy();
